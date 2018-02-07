8db6ca140a8204e5b4e9e864c2f1196c260fc4ac
Dustin Sallings <dustin@spy.net>
2018-02-06 23:00:23 -0800

    Nightly has a newer tasty-quickcheck, so compensate

--- test/Spec.hs
+++ test/Spec.hs
@@ -72,8 +72,8 @@ someSym :: SymEither (Int, Int, Int)
 someSym = undefined
 
 -- TODO:  Fix when tasty-quickcheck 0.9.1 is available
-testProperties :: TestName -> [(String, Property)] -> TestTree
-testProperties name = testGroup name . map (uncurry testProperty)
+testProperties' :: TestName -> [(String, Property)] -> TestTree
+testProperties' name = testGroup name . map (uncurry testProperty)
 
 tests :: [TestTree]
 tests = [
@@ -82,9 +82,9 @@ tests = [
   testCase "success response parsing" testGoodResponseParsing,
   testCase "success response parsing with err" testGoodResponseParsingBadInt,
 
-  testProperties "SymEither functor" (unbatch $ functor someSym),
-  testProperties "SymEither applicative" (unbatch $ applicative someSym),
-  testProperties "SymEither monad" (unbatch $ monad someSym),
+  testProperties' "SymEither functor" (unbatch $ functor someSym),
+  testProperties' "SymEither applicative" (unbatch $ applicative someSym),
+  testProperties' "SymEither monad" (unbatch $ monad someSym),
   testProperty "SymEither fromEither" (from_either_prop 0 :: Either Int Int -> Bool),
   testProperty "SymEither toEither" (to_either_prop :: SymEither Int -> Bool),
   testProperty "SymEither left" (left_prop :: SymEither Int -> Bool)
